03c8a362a208060ae85209252c3d0430e158b66d
Sean Straus <Sstraus21@gmail.com>
2015-08-19 14:16:33 -0500

    created a test webpage to experiment with

---
+++ experiments/filler.js
@@ -0,0 +1,36 @@
+import spock.lang.*
+ 
+class ExceptionHandlerSpec extends Specification {
+ 
+	@FailsWith(NumberFormatException)
+	def "Expect a failure using annotation"() {
+	expect: Integer.parseInt("99", 8)
+	}
+	 
+	def "Expect a failure"() {
+	when:
+	Integer.parseInt("99", 8)
+	 
+	then:
+	NumberFormatException e = thrown()
+	e.message == 'For input string: "99"'
+	}   
+	 
+	def "Expect a failure not true"() {
+	when:
+	Integer.parseInt("-2147483648", 10)then:
+	notThrown(NumberFormatException )
+	}
+}
+
+var login = function(){
+	console.log("whoop!");
+};
+
+var playNow = function(){
+	console.log("aggle aggle!");
+};
+
+var signup = function(){
+	console.log("snarf!");
+};
