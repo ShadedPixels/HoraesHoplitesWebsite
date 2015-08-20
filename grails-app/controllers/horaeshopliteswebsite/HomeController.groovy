7cb24934dc44c94f66b03ad03f21acd009e49880
Anton <AntonKostyuchenko0112358@gmail.com>
2015-08-19 21:18:49 -0500

    Clean up Home Controller and add a little documentation to the TailService

--- grails-app/controllers/horaeshopliteswebsite/HomeController.groovy
+++ grails-app/controllers/horaeshopliteswebsite/HomeController.groovy
@@ -1,26 +1,8 @@
 package horaeshopliteswebsite
 
-import grails.converters.JSON
-import grails.converters.XML
-import groovy.transform.TailRecursive;
-
-
 class HomeController {
 
- 	def log_array = []
-
-	def tailService
-
 	def index() {
-			
-//		def file = new File("/tmp/woodpecker.log")
-//
-//		tailService.tail("test", file){
-//				log_array << it
-//				print log_array
-//		}
-//
-//		tailService.stop("test")
 		
 		def builds = [
 			["Build #1.0", "Lorem ipsum dolor sit amet, consectetur \
@@ -35,15 +17,6 @@ class HomeController {
 		
 		return [builds: builds]
 		
-
 	}
 
-    def get_log(){
-		render log_array as JSON
-    }
-
-    def blah(){
-		def message = ["id": params.id, "something": params.something]
-		render message as JSON
-    }
 }
