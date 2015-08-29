e54b62474ab863b54188862b85ad7c3abf1d13c4
Anton <AntonKostyuchenko0112358@gmail.com>
2015-08-29 13:56:03 -0500

    Implement initial methods of GameService, and unit tests
    Methods: create, getById, list

--- grails-app/services/horaeshopliteswebsite/GameService.groovy
+++ grails-app/services/horaeshopliteswebsite/GameService.groovy
@@ -1,31 +1,61 @@
 package horaeshopliteswebsite
 
+import java.util.logging.Logger;
+
 import grails.transaction.Transactional
 
 @Transactional
 class GameService {
+	
+	def userService
 
 	@Transactional
-    def create(){
+    def create(String name, String creator_username){
 	/*
 	 * creates new game
 	 * return:
 	 * 		id of the game just created
 	 */
+		User creator = userService.getByUsername(creator_username)
+		if(creator == null){
+			log.info("User ${creator_username} not found")
+			return -1
+		}
+		
+		Game game = new Game(name: name, creator: creator).save();
+		game.addToParticipants(creator)
+		game = game.save(flush:true)
+		if(game == null){
+			// if failed to save
+			return -1
+		}
+		return game.id
 	}
 	
 	@Transactional
-	def join(){
+	def join(game_id, String username){
+		Game game = Game.get(game_id)
+		User user = User.findByUsername(username)
+		
+		if(game == null || user == null){ // if can't find one of them
+			return false
+		}
+		
+		if(user in game.participants){
+			return true
+		}
+		game.addToParticipants(user)
+		return !!game.save(flush: true)
 		
 	}
 	
 	@Transactional(readOnly = true)
 	def list(){
-		
+		return Game.list().collect {[it.id, it.name]}
 	}
 	
 	@Transactional(readOnly = true)
-	def getById(int id){
-		
+	def getById(id){
+		return Game.get(id)
 	}
 }
--- test/unit/horaeshopliteswebsite/GameServiceSpec.groovy
+++ test/unit/horaeshopliteswebsite/GameServiceSpec.groovy
@@ -1,5 +1,6 @@
 package horaeshopliteswebsite
 
+import grails.test.mixin.Mock
 import grails.test.mixin.TestFor
 import spock.lang.Specification
 
@@ -7,14 +8,55 @@ import spock.lang.Specification
  * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
  */
 @TestFor(GameService)
+@Mock([User, Game, UserService])
 class GameServiceSpec extends Specification {
-
+	
     def setup() {
     }
 
     def cleanup() {
     }
 
-    void "test something"() {
+    void "test game creation"() {
+		setup:	
+			User john = new User(username: "John").save(flush: true, failOnError: true)
+			
+		when:
+			def game_id = service.create("game1", "John")
+			service.userService.getByUsername(_) >> john;
+			
+		then:
+			game_id >= 0 // not an error flag 
+			
+			Game.list().size() == 1
+			Game game = Game.findByName("game1")
+			game != null // game was created
+			
+			game_id == game.id // id that we recieved from create function equals actual id
+			
+			game == service.getById(game_id)
+			
+			service.list() == [[game.id, game.name]]
+			
+			game.creator == john // creator is correct
+			
+			
     }
+	
+	void "test joining game"(){
+		
+		setup:
+			User creator = new User(username: "creator").save()
+			User participant = new User(username: "participant").save(flush: true, failOnError: true)
+			
+			def game_id = service.create("game", "creator")
+			Game game = Game.get(game_id)
+		
+		when: "new participant added"
+			boolean success = service.join(game.id, "participant")
+			game = Game.findByName("game")
+		then:
+			success == true
+			game.refresh().participants.sort() == [creator, participant].sort()
+	}
 }
