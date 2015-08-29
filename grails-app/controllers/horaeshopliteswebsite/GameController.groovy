2e05e1db6b5a66c3ef775677dbaa7ac4a57e77ce
Anton <AntonKostyuchenko0112358@gmail.com>
2015-08-29 16:04:41 -0500

    Complete several actions for GameController.ajax_response
    * Didn't write any tests (will wait w/ it until we are more sure about
        structure of the controller)
    * Actions implemented: 'create user', 'create game', 'join game', 'list
        users', 'list games'

--- grails-app/controllers/horaeshopliteswebsite/GameController.groovy
+++ grails-app/controllers/horaeshopliteswebsite/GameController.groovy
@@ -5,6 +5,9 @@ import grails.converters.JSON
 import org.codehaus.groovy.grails.web.json.JSONObject
 
 class GameController {
+	
+	def userService
+	def gameService
 
     def index() { 
 		def map = []
@@ -23,17 +26,31 @@ class GameController {
 		switch (operation){
 			case 'login':
 				break
+				
 			case 'create user':
+				def(success, user_id) = userService.create(data.username)
+				reply << ['success': success, 'user_id': user_id]
 				break
+				
 			case 'create game':
+				def game_name = data.game_name || ""
+				def creator_username = data.creator_username
+				def(success, game_id) = gameService.create(game_name, creator_username)
+				reply << ['success': success, 'game_id': game_id]
 				break
+				
 			case 'play singleplayer':
 				break
 			case 'join game':
+				def success = gameService.join(data.game_id, data.username) // replace with session info later
+				reply << ['success': success]
 				break
+				
 			case 'list users':
+				reply << ["user_array": userService.list()]
 				break
 			case 'list games':
+				reply << ["game_array": gameService.list()]
 				break
 				
 			case 'default':
